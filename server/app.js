ab88466aee8b6789a009b97f100cb424c9491125
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-05-20 16:33:24 +0200

    Create paths, import API

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const PORT = 5005;
+const PORT = 5012;
+const data = require("./package.json");
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -28,6 +31,13 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
+
+ app.get("/api/cohorts", (req, res) =>{
+  res.json(cohorts);
+});
+app.get("/api/students", (req, res) =>{
+  res.json(students);
+})
 
 
 // START SERVER
